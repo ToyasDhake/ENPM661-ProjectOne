270ccd68160b29b3611e8666066baca46194f2f2
Toyas Dhake <toyasdhake@gmail.com>
2020-02-15 13:59:45 -0500

    Implemented A* for solving in texted base.

--- TextEightPuzzle.py
+++ TextEightPuzzle.py
@@ -1,8 +1,53 @@
 from itertools import chain, product
+from collections import deque
+from copy import deepcopy
+
+
+class Node:
+    def __init__(self, currentBorad, parent=None, action=None):
+        self.currentBorad = currentBorad
+        self.parent = parent
+        self.action = action
+        if (self.parent != None):
+            self.g = parent.g + 1
+        else:
+            self.g = 0
+        self.f = self.g + currentBorad.score()
+
+    @property
+    def path(self):
+        node, p = self, []
+        while node:
+            p.append(node)
+            node = node.parent
+        yield from reversed(p)
+
+    @property
+    def actions(self):
+        return self.currentBorad.availbleMoves()
 
 
 class Solver:
-    pass
+    def __init__(self, gameBoard):
+        self.start = gameBoard
+
+    def solve(self):
+        queue = deque()
+        queue.append(Node(self.start))
+        seen = set()
+        seen.add(queue[0].currentBorad)
+        while queue:
+            queue = deque(sorted(list(queue), key=lambda node: node.f))
+            node = queue.popleft()
+            if node.currentBorad.score() == 0:
+                return node.path
+
+            for action in node.actions:
+                child = Node(node.currentBorad.move(action), node, action)
+
+                if child.currentBorad not in seen:
+                    queue.appendleft(child)
+                    seen.add(child.currentBorad)
 
 
 class Puzzle:
@@ -45,16 +90,40 @@ class Puzzle:
 
     def availbleMoves(self):
         row, col = self.getLocationOfBlank(self.gameBoard)
-        list = [0] * 4
+        list = []
         if row is not 0:
-            list[0] = 1
+            list.append("U")
         if row is not 2:
-            list[1] = 1
+            list.append("D")
         if col is not 0:
-            list[2] = 1
+            list.append("L")
         if col is not 2:
-            list[3] = 1
+            list.append("R")
         return list
+
+    def move(self, val):
+        row, col = self.getLocationOfBlank(self.gameBoard)
+        newGameBoard = deepcopy(self)
+        if val is 'U':
+            newGameBoard.gameBoard[row][col], newGameBoard.gameBoard[row - 1][col] = newGameBoard.gameBoard[row - 1][col], newGameBoard.gameBoard[row][col]
+        elif val is 'D':
+            newGameBoard.gameBoard[row][col], newGameBoard.gameBoard[row + 1][col] = newGameBoard.gameBoard[row + 1][col], newGameBoard.gameBoard[row][col]
+        elif val is 'L':
+            newGameBoard.gameBoard[row][col], newGameBoard.gameBoard[row][col - 1] = newGameBoard.gameBoard[row][col- 1], newGameBoard.gameBoard[row][col]
+        elif val is 'R':
+            newGameBoard.gameBoard[row][col], newGameBoard.gameBoard[row][col + 1] = newGameBoard.gameBoard[row][col + 1], newGameBoard.gameBoard[row][col]
+        return newGameBoard
+
+    def actions(self):
+        pass
+
 
 gameBoard = [[1, 8, 2], [0, 4, 3], [7, 6, 5]]
 puzzle = Puzzle(gameBoard)
+solver = Solver(puzzle)
+solved = solver.solve()
+steps = 0
+for node in solved:
+    print(node.action)
+    print(node.currentBorad.gameBoard)
+    steps += 1
